d3e1d6396e7d821744ebfe456df09b07a08cfed2
fancyzhong <fancyzhong@tencent.com>
2020-04-07 14:10:46 +0800

    types: update store.ts

--- src/types/store.ts
+++ src/types/store.ts
@@ -68,7 +68,7 @@ export type CtxMSConn<P, M extends Modules, St, Conn extends Modules, Se = {}> =
 export type CtxConn<P, Conn extends Modules, Se = {}> = ICtx<RootState, RootReducer, RootComputed, P, {}, MODULE_DEFAULT, Conn, Se>;
 
 // default系列，没有指定连接模块的组件默认属于$$default模块
-export type CtxDe<P, Se = {}> = ICtx<RootState, RootReducer, RootComputed, P, {}, MODULE_DEFAULT, MODULE_VOID, Se>;
-export type CtxDeS<P, St = {}, Se = {}> = ICtx<RootState, RootReducer, RootComputed, P, St, MODULE_DEFAULT, MODULE_VOID, Se>;
-export type CtxDeConn<P, Conn extends Modules, Se = {}> = ICtx<RootState, RootReducer, RootComputed, P, {}, MODULE_DEFAULT, Conn, Se>;
-export type CtxDeSConn<P, St, Conn extends Modules, Se = {}> = ICtx<RootState, RootReducer, RootComputed, P, St, MODULE_DEFAULT, Conn, Se>;
+export type CtxDe<P, Se = {}> = CtxM<P, MODULE_DEFAULT, Se>;
+export type CtxDeS<P, St = {}, Se = {}> = CtxMS<P, MODULE_DEFAULT, St, Se>;
+export type CtxDeConn<P, Conn extends Modules, Se = {}> = CtxMConn<P, MODULE_DEFAULT, Conn, Se>;
+export type CtxDeSConn<P, St, Conn extends Modules, Se = {}> = CtxMSConn<P, MODULE_DEFAULT, St, Conn, Se>;
